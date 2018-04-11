898395161a123652b0736203ee1bb3ba202d158d
Mads <Mads@10.50.137.1>
2018-04-11 08:13:57 +0200

    Player updated with get & set LookAndFeel updated with decriptions

--- src/tag1/Player.java
+++ src/tag1/Player.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tag1;
 
-/**
- *
- * @author Mark
- */
+
 public class Player {
     private String name;
     private int health;
@@ -17,6 +9,24 @@ public class Player {
         this.name = name;
         this.health = health;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    
+    
     
     
     
